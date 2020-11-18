dde21233879800b4a768fd39c46c801200d9a431
kokyoungwoo <bizdb@kakao.com>
2020-11-18 16:35:13 +0900

    hash scroll animate update
    commit

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(function(){
   //script 입력영역
+  $("nav ul li a,.back_to_top a,.footer_top a,a.button").click(function(e){
+    // console.log($(this.hash).offset().top)
+    console.log($(this.hash).offset())
+    var thisElem = $(this.hash);
+    $("html,body").stop();
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);
+
+    //e.preventDefault();
+    return false;
+
+  });
+
 });
